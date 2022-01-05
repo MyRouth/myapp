b84b78b4a35d237a4bbd2057b35f2a11aa2de636
Ashu <ash.kelkar.poly@gmail.com>
2022-01-05 19:00:05 +0530

    my react apps

---
+++ helloworld/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import hello from './components/hello';
+function App() {
+  return (
+    <div className="App">
+      {/* <hello /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ helloworld/src/components/hello.js
@@ -0,0 +1,5 @@
+import React from 'react'
+function hello(){
+    return <h1>hello world</h1>
+}
+export default hello
